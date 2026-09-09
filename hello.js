e80d0a9f3f52bc49e6ff27c130d6754764e8d8d8
Yeasin Arafat <yeasinarafat5331@gmail.com>
2026-09-09 13:09:17 +0600

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,5 +1,24 @@
+const startTime = performance.now();
 
-        function hi(name) {
-           return "hai " + name;
-        }
-        
+// ... code you want to measure ...
+const numbers = [10, 20, 30];
+let maxNumber = numbers[0];
+let minNumber = numbers[0];
+
+for (const number of numbers) {
+    if (number > maxNumber) {
+        maxNumber = number;
+    }
+
+    if (number < minNumber) {
+        minNumber = number;
+    }
+}
+
+console.log("The maximum number is: " + maxNumber);
+console.log("The minimum number is: " + minNumber);
+
+const endTime = performance.now();
+const timeTaken = endTime - startTime;
+
+console.log(`Execution time: ${timeTaken} milliseconds`);
